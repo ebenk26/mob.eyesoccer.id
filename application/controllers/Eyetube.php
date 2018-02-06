53d84449a3f9c4be97d6b604e2e335d720a2e47b
Kharizuno <kharizuno@yahoo.com>
2018-02-06 14:48:06 +0700

    ubah tube

--- application/controllers/Eyetube.php
+++ application/controllers/Eyetube.php
@@ -35,6 +35,7 @@ class Eyetube extends CI_Controller {
 	    $data['meta_keyword'] = $this->config->item('meta_keyword');
 	    
 	    $this->load->view($this->__theme().'template', $data);
+	}
     }
 
     function detail($slug = '')
