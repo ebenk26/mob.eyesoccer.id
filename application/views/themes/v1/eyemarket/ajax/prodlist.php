399c530db3b5fbabfa1264163cdc7ff317e804f9
sofyanwaldi96@gmail.com <sofyanwaldi96@gmail.com>
2018-02-06 14:35:33 +0700

    eyemarket home done (sofyan)

--- application/views/themes/v1/eyemarket/ajax/prodlist.php
+++ application/views/themes/v1/eyemarket/ajax/prodlist.php
@@ -1,15 +1,12 @@
 <?php
-p($prodlist);
-
- 
 
  foreach($prodlist as $k => $v){
 
- 	echo "<a href=\"\">
+ 	echo "<a href=\"".base_url()."detail/{$v['toko']}/{$v['title_slug']}\">
                     <div class=\"em-box-content\">
                         <img src=\"".imgCache($v['image1'])."\" alt=\"{$v['nama']}\">
                         <h1>{$v['nama']}</h1>
-                        <p class=\"prices\"> ".number_format($produk['harga'],0,',','.')."</p>
+                        <p class=\"prices\"> ".number_format($v['harga'],0,',','.')."</p>
                         <button type=\"text\" class=\"beli\">Beli</button>
                     </div>
                 </a>";
