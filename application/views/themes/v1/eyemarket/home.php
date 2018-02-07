6405d8f00bf59836e0c98bed6d3245290dbe55bf
Muhammad Robi <muhammadrobi65@gmail.com>
2018-02-07 14:27:03 +0700

    edit logo eyesoccer for direct to home
    --robi

--- application/views/themes/v1/eyemarket/home.php
+++ application/views/themes/v1/eyemarket/home.php
@@ -1,6 +1,8 @@
 <nav>
     <i id="menu" class="material-icons f-l">menu</i>
-    <img src="https://www.eyesoccer.id/img/logo2.png" alt="">
+    <a href="<?= base_url(); ?>">
+	    <img src="https://www.eyesoccer.id/img/logo2.png" alt="">
+	</a>
     <i id="src" class="material-icons f-r">search</i>
 </nav>
     <!-- TRENDING --> 
