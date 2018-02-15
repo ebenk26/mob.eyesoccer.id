4598e7074a6664128e02db49d79162716e11094e
rizki <rizki@intelix.co.id>
2018-02-15 12:42:36 +0700

    update eyenews related news href

--- application/views/themes/v1/eyenews/ajax/related_news.php
+++ application/views/themes/v1/eyenews/ajax/related_news.php
@@ -4,14 +4,16 @@ if ($newsdetail){
 	foreach($newsdetail->data->related_news as $dt)
 	{
 ?>
-	<div class="etube-content etube-content-detail pd-t-10">
-		<div class="v-etube v-etube-detail">
-			<img src="<?php echo $dt->url_pic.'/small';?>" alt="">
+	<a href="<?php echo $this->library->urltomob($dt->url);?>">
+		<div class="etube-content etube-content-detail pd-t-10">
+			<div class="v-etube v-etube-detail">
+				<img src="<?php echo $dt->url_pic.'/small';?>" alt="">
+			</div>
+			<h1><?php echo $dt->title;?></h1>
+			<span><?php echo relative_time($this->library->datenormal($dt->publish_on)); ?> lalu</span>
+			<span><?php echo $dt->news_view; ?> view</span>
 		</div>
-		<h1><?php echo $dt->title;?></h1>
-		<span><?php echo relative_time($this->library->datenormal($dt->publish_on)); ?> lalu</span>
-		<span><?php echo $dt->news_view; ?> view</span>
-	</div>
+	</a>
 <?php
 	}
 }
