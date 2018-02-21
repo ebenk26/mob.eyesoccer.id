a7c81efe099e5f13763d735d5d7ef715a28c4e44
sofyanwaldi96@gmail.com <sofyanwaldi96@gmail.com>
2018-02-21 15:14:38 +0700

    blah

--- application/views/themes/v1/eyeme/ajax/me_a_list.php
+++ application/views/themes/v1/eyeme/ajax/me_a_list.php
@@ -4,7 +4,7 @@ $me = json_decode($eyeme);
 foreach($me->data as $k => $v){
 	$url = $v->url_img.'/small';
 
-	echo  '<a href="'.$v->url.'">
+	echo  '<a href="'.str_replace('https://www', 'http://m', $v->url).'">
 			<div class="eme-box">
         		<img src="'.$url.'" alt="'.$v->img_caption.'">
     	 	</div>
