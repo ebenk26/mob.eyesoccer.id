4cf770f8440a903efbdf09a148511c11f860b4ef
sofyanwaldi96@gmail.com <sofyanwaldi96@gmail.com>
2018-02-21 15:10:16 +0700

    blah

--- application/views/themes/v1/eyeme/ajax/me_a_list.php
+++ application/views/themes/v1/eyeme/ajax/me_a_list.php
@@ -2,9 +2,9 @@
 
 $me = json_decode($eyeme);
 foreach($me->data as $k => $v){
-	$url = urltobetame($v->url_img);
+	$url = $v->url_img.'/small';
 
-	echo  '<a href="'.$v->url.'/small">
+	echo  '<a href="'.$v->url.'">
 			<div class="eme-box">
         		<img src="'.$url.'" alt="'.$v->img_caption.'">
     	 	</div>
