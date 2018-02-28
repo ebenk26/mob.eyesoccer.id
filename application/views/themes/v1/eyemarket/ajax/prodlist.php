e8a234efc5d6af11166ca7d15bf51e138ecb437e
Muhammad Robi <muhammadrobi65@gmail.com>
2018-02-28 11:13:36 +0700

    edit eyemarket receh
    --robi

--- application/views/themes/v1/eyemarket/ajax/prodlist.php
+++ application/views/themes/v1/eyemarket/ajax/prodlist.php
@@ -2,7 +2,7 @@
 
  foreach($prodlist as $k => $v){
 
- 	echo "<a href=\"".base_url()."detail/{$v['toko']}/{$v['title_slug']}\">
+ 	echo "<a href=\"".base_url()."eyemarket/detail/{$v['toko']}/{$v['title_slug']}\">
                     <div class=\"em-box-content\">
                         <div class=\"img-prod\">
                             <img src=\"".MARKETIMG.$v['image1']."\" alt=\"{$v['nama']}\">
