9120a1e870c43d02882e94dc7cb291f5ea373445
rizki <rizki@intelix.co.id>
2018-03-01 11:55:28 +0700

    update google analytic & myhelper crumb

--- application/views/themes/v1/template.php
+++ application/views/themes/v1/template.php
@@ -3,6 +3,13 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-P7HQLCC');</script>
+	<!-- End Google Tag Manager -->
     <title><?php echo $title; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -61,6 +68,10 @@
     
 </head>
 <body>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P7HQLCC"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
     <?php
         $data['folder'] = $folder;
         $this->load->view($folder.'widget/navigator', $data);
