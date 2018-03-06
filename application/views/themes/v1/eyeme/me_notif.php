b14703b76fba75cfcb3414b34f28eee2ecdb883f
sofyanwaldi96@gmail.com <sofyanwaldi96@gmail.com>
2018-03-06 10:39:10 +0700

    perbaikan notifikasi eyeme

--- application/views/themes/v1/eyeme/me_notif.php
+++ application/views/themes/v1/eyeme/me_notif.php
@@ -42,51 +42,8 @@
                 </td>
                 <td><span class="gr" style="width: 25px;height: 25px;border-radius: 50%;display: block;"></span></td>
             </tr>
-            <tr>
-                <td>
-                <span class="gr" style="width: 25px;height: 25px;border-radius: 50%;display: block;"></span>
-                </td>
-                <td>
-                    <a href="" class="gr">Lorem Ipsum lorem ipsum</a>
-                </td>
-                <td><span class="gr" style="width: 25px;height: 25px;border-radius: 50%;display: block;"></span></td>
-            </tr>
-            <tr>
-                <td>
-                    <span class="gr" style="width: 25px;height: 25px;border-radius: 50%;display: block;"></span>
-                </td>
-                <td>
-                    <a href="" class="gr">Lorem Ipsum lorem ipsum</a>
-                </td>
-                <td><span class="gr" style="width: 25px;height: 25px;border-radius: 50%;display: block;"></span></td>
-            </tr>
-            <tr>
-                <td>
-                <span class="gr" style="width: 25px;height: 25px;border-radius: 50%;display: block;"></span>
-                </td>
-                <td>
-                    <a href="" class="gr">Lorem Ipsum lorem ipsum</a>
-                </td>
-                <td><span class="gr" style="width: 25px;height: 25px;border-radius: 50%;display: block;"></span></td>
-            </tr>
-            <tr>
-                <td>
-                    <span class="gr" style="width: 25px;height: 25px;border-radius: 50%;display: block;"></span>
-                </td>
-                <td>
-                    <a href="" class="gr">Lorem Ipsum lorem ipsum</a>
-                </td>
-                <td><span class="gr" style="width: 25px;height: 25px;border-radius: 50%;display: block;"></span></td>
-            </tr>
-            <tr>
-                <td>
-                <span class="gr" style="width: 25px;height: 25px;border-radius: 50%;display: block;"></span>
-                </td>
-                <td>
-                    <a href="" class="gr">Lorem Ipsum lorem ipsum</a>
-                </td>
-                <td><span class="gr" style="width: 25px;height: 25px;border-radius: 50%;display: block;"></span></td>
-            </tr>
+           
+           
         </table>
     </div>
 </div>
