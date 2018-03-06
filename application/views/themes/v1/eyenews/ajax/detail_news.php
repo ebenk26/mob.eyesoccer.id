3d4b322538d5785d857517cd03aa8a7c386240cd
rizki <rizki@intelix.co.id>
2018-03-06 15:04:58 +0700

    update checkout detail news adsense

--- application/views/themes/v1/eyenews/ajax/detail_news.php
+++ application/views/themes/v1/eyenews/ajax/detail_news.php
@@ -104,16 +104,6 @@ if ($newsdetail){
 				</a>
 			</div>
 		</div>
-		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-		<ins class="adsbygoogle"
-			 style="display:block; text-align:center;"
-			 data-ad-layout="in-article"
-			 data-ad-format="fluid"
-			 data-ad-client="ca-pub-7635854626605122"
-			 data-ad-slot="3624793128"></ins>
-		<script>
-			 (adsbygoogle = window.adsbygoogle || []).push({});
-		</script>
 	</div>
 	<script>
 		$(document).ready(function(){
