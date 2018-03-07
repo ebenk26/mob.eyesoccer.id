4589cc2c2023a42a66426bf2d85ef277619bcbf4
sofyanwaldi96@gmail.com <sofyanwaldi96@gmail.com>
2018-03-07 14:25:17 +0700

    perbaikan melis

--- application/views/themes/v1/eyeme/ajax/me_a_list.php
+++ application/views/themes/v1/eyeme/ajax/me_a_list.php
@@ -3,8 +3,9 @@
 $me = json_decode($eyeme);
 foreach($me->data as $k => $v){
 	$url = $v->url_img.'/small';
+	$replace = str_replace('https://www', 'http://m', $v->url);
 
-	echo  '<a href="'.str_replace('https://www', 'http://m', $v->url).'">
+	echo  '<a href="'.($this->session->member ? $replace : LOGINURL.'?from=eyeme').'">
 			<div class="eme-box">
         		<img src="'.$url.'" alt="'.$v->img_caption.'">
     	 	</div>
