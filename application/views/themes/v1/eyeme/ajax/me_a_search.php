bbe4ad66656785bef41e428448318886b423b565
faradiland <fndanopa@gmail.com>
2018-03-09 17:39:29 +0700

    search blm rapi

--- application/views/themes/v1/eyeme/ajax/me_a_search.php
+++ application/views/themes/v1/eyeme/ajax/me_a_search.php
@@ -5,7 +5,12 @@ if(count($res->data) > 0 ){
 	echo '<div style="height: 300px">';
 	foreach($res->data as $k => $v){?>
 		<div class="pp">
-			<a href="<?php echo MEPROFILE.$v->username?>"><img style="border-radius:100%;height: 50px;width: 50px" src="<?php echo $v->url_pic?>" onerror="this.src='<?php echo DPIC?>'"/><?php echo $v->username.'  ('.$v->name.')'?></a>
+			<a href="<?php echo MEPROFILE.$v->username?>">
+				<div class="pp-search">
+					<img src="<?php echo $v->url_pic?>" onerror="this.src='<?php echo DPIC?>'"/>
+				</div>
+			<span class="container"><?php echo $v->username.'  ('.$v->name.')'?></span>
+			</a>
 		</div>
 
 		<?php 
