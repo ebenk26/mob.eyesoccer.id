7c16fc78821ee7fadda8ad83d0ff780a2fa32707
unuha26 <unuha26@users.noreply.github.com>
2018-03-13 16:45:28 +0700

    update iklan

--- application/views/themes/v1/template.php
+++ application/views/themes/v1/template.php
@@ -88,6 +88,13 @@
             });
         });
     </script>-->
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-7635854626605122",
+            enable_page_level_ads: true
+        });
+    </script>
 </head>
 <body>
 	<!-- Google Tag Manager (noscript) -->
