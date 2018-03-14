ee753764f18c96bd151cc6765a0d43243aad83e7
Kharizuno <kharizuno@yahoo.com>
2018-03-14 09:42:23 +0700

    detail terkait

--- application/views/themes/v1/eyenews/detail.php
+++ application/views/themes/v1/eyenews/detail.php
@@ -1,52 +1,53 @@
 <div class="crumb" style="margin-bottom: 0px;">
-	<ul>
-		<li>Home</li>
-		<li>EyeNews</li>
-	</ul>
+    <ul>
+        <li>Home</li>
+        <li>EyeNews</li>
+    </ul>
 </div>
 <div id="reqdetail" class='loaddetail' action="eyenews" loading="off" clean="clsdetail">
-		<div id='clsdetail'>
-			<script>
-				$(document).ready(function(){
-					$(window).on('load',function(){
-						ajaxOnLoad('loaddetail');
-					});
-				});
-			</script>
-		</div>
-		<input type='hidden' name='fn' value='detail_news' class='cinput'>
-		<input type='hidden' name='slug' value='<?php echo $slug; ?>' class='cinput'>
-<div class="container enews-detail">
-	<h1 class="gr">Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet</h1>
-	<img src="" alt="" width="100%" height="200px" class="fafa">
-	<div class="container profile-writer b-b-1">
-		<div class="container">
-			<div class="img-radius gr" style="border: 15px solid;border-color: white !important;box-sizing: content-box;margin: -5px 0 0 -15px;">
-				<img src="" alt="">
-			</div>
-			<h3 class="gr">Aditya Gilang</h3>
-			<span class="gr">09 nov 2017, 21.40 WIB</span>                
-		</div>
-		<!-- <div class="container tx-c share">
-			<i class="fa fa-facebook" aria-hidden="true"></i>
-			<i class="fa fa-twitter" aria-hidden="true"></i>
-			<i class="fa fa-google-plus-official" aria-hidden="true"></i>
-			<i class="fa fa-envelope-o" aria-hidden="true"></i>
-			<i class="fa fa-comment openComment" aria-hidden="false"><div class="openComment">16</div></i>
-		</div> -->
-	</div>
-	<div class="container-94">
-		<p style="margin-bottom: 50px;font-size: .9em;" class="gr">
-			Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet
-		</p>
-	</div>
-	</div>
-	<!-- EMOTICON -->
-	<!-- <div class="container-94" style="background-color: white;padding: 10px 0px 20px;">
+    <div id='clsdetail'>
+        <script>
+            $(document).ready(function () {
+                $(window).on('load', function () {
+                    ajaxOnLoad('loaddetail');
+                });
+            });
+        </script>
+    </div>
+    <input type='hidden' name='fn' value='detail_news' class='cinput'>
+    <input type='hidden' name='slug' value='<?php echo $slug; ?>' class='cinput'>
+    <div class="container enews-detail">
+        <h1 class="gr">Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet</h1>
+        <img src="" alt="" width="100%" height="200px" class="fafa">
+        <div class="container profile-writer b-b-1">
+            <div class="container">
+                <div class="img-radius gr" style="border: 15px solid;border-color: white !important;box-sizing: content-box;margin: -5px 0 0 -15px;">
+                    <img src="" alt="">
+                </div>
+                <h3 class="gr">Aditya Gilang</h3>
+                <span class="gr">09 nov 2017, 21.40 WIB</span>
+            </div>
+            <!-- <div class="container tx-c share">
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+                <i class="fa fa-google-plus-official" aria-hidden="true"></i>
+                <i class="fa fa-envelope-o" aria-hidden="true"></i>
+                <i class="fa fa-comment openComment" aria-hidden="false"><div class="openComment">16</div></i>
+            </div> -->
+        </div>
+        <div class="container-94">
+            <p style="margin-bottom: 50px;font-size: .9em;" class="gr">
+                Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet
+                Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet
+            </p>
+        </div>
+    </div>
+    <!-- EMOTICON -->
+    <!-- <div class="container-94" style="background-color: white;padding: 10px 0px 20px;">
 		<div class="col-3 col-emoji">
 			<a href="">
 				<div class="img-box">
-					<img src="<?php echo SUBCDN."assets/$folder/" ?>img/emoji/bangga.png" alt="">
+					<img src="<?php echo SUBCDN . "assets/$folder/" ?>img/emoji/bangga.png" alt="">
 				</div>
 				<span>10%</span>
 				<span>bangga</span>
@@ -55,7 +56,7 @@
 		<div class="col-3 col-emoji">
 			<a href="">
 				<div class="img-box">
-					<img style="margin-top:  -6px;" src="<?php echo SUBCDN."assets/$folder/" ?>img/emoji/bingung.png" alt="">
+					<img style="margin-top:  -6px;" src="<?php echo SUBCDN . "assets/$folder/" ?>img/emoji/bingung.png" alt="">
 				</div>
 				<span>10%</span>
 				<span>bingung</span>
@@ -64,7 +65,7 @@
 		<div class="col-3 col-emoji">
 			<a href="">
 				<div class="img-box box-img-90">
-					<img src="<?php echo SUBCDN."assets/$folder/" ?>img/emoji/gembira.png" alt="">
+					<img src="<?php echo SUBCDN . "assets/$folder/" ?>img/emoji/gembira.png" alt="">
 				</div>
 				<span>10%</span>
 				<span>gembira</span>
@@ -73,7 +74,7 @@
 		<div class="col-3 col-emoji">
 			<a href="">
 				<div class="img-box">
-					<img src="<?php echo SUBCDN."assets/$folder/" ?>img/emoji/sedih.png" alt="">
+					<img src="<?php echo SUBCDN . "assets/$folder/" ?>img/emoji/sedih.png" alt="">
 				</div>
 				<span>10%</span>
 				<span>sedih</span>
@@ -82,7 +83,7 @@
 		<div class="col-3 col-emoji">
 			<a href="">
 				<div class="img-box">
-					<img style="margin-top:  -6px;" src="<?php echo SUBCDN."assets/$folder/" ?>img/emoji/suka.png" alt="">
+					<img style="margin-top:  -6px;" src="<?php echo SUBCDN . "assets/$folder/" ?>img/emoji/suka.png" alt="">
 				</div>
 				<span>10%</span>
 				<span>suka</span>
@@ -91,7 +92,7 @@
 		<div class="col-3 col-emoji">
 			<a href="">
 				<div class="img-box">
-					<img src="<?php echo SUBCDN."assets/$folder/" ?>img/emoji/takut.png" alt="">
+					<img src="<?php echo SUBCDN . "assets/$folder/" ?>img/emoji/takut.png" alt="">
 				</div>
 				<span>10%</span>
 				<span>takut</span>
@@ -100,7 +101,7 @@
 		<div class="col-3 col-emoji">
 			<a href="">
 				<div class="img-box">
-					<img src="<?php echo SUBCDN."assets/$folder/" ?>img/emoji/terhibur.png" alt="">
+					<img src="<?php echo SUBCDN . "assets/$folder/" ?>img/emoji/terhibur.png" alt="">
 				</div>
 				<span>10%</span>
 				<span>terhibur</span>
@@ -109,7 +110,7 @@
 		<div class="col-3 col-emoji">
 			<a href="">
 				<div class="img-box">
-					<img src="<?php echo SUBCDN."assets/$folder/" ?>img/emoji/terkejut.png" alt="">
+					<img src="<?php echo SUBCDN . "assets/$folder/" ?>img/emoji/terkejut.png" alt="">
 				</div>
 				<span>10%</span>
 				<span>terkejut</span>
@@ -117,94 +118,96 @@
 		</div>
 	</div> -->
 </div>
-	<!-- IKLAN -->
-	<div class="container">
-		<div class="box-flyad">
-			<div class="flyad">
-				<div class="m-0">
-				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-				<!-- Eyesoccer 4#mobileNewsDetailBackGround -->
-				<ins class="adsbygoogle"
-					style="display:block"
-					data-ad-client="ca-pub-7635854626605122"
-					data-ad-slot="6050658173"
-					data-ad-format="auto"></ins>
-				<script>
-				(adsbygoogle = window.adsbygoogle || []).push({});
-				</script>
-					<!-- <span style="font-size: 7em;font-weight: 100;position: fixed;text-align: center;top: 20%;color: white;">ini iklan yaa</span> -->
-					<!-- <img src="http://www.desktopimages.org/pictures/2014/0118/1/red-smoke-wallpaper-hd-5449.jpg" alt=""> -->
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="container enews-detail">
-	<div class="container m-b-15" style="margin-top: -35px !important; background-color: #fafafa;">
-	<h3 class="h3-red">VIDEO</h3>
-	<div class="carousel slide t-30 over-x">
-		<div role="listbox" class="carousel-inner w-max">
-			<div class="box item active no-decoration">
-				<div id="reqtube" class='loadtube' action="eyetube" loading="on" clean="clstube">
-					<div id='clstube'>
-						<script>
-							$(document).ready(function(){
-								$(window).on('load',function(){
-									ajaxOnLoad('loadtube');
-								});
-							});
-						</script>
-					</div>
-					<input type='hidden' name='fn' value='list_tube_eyenews' class='cinput'>
-					<a href="">
-						<div class="video-thumb fafa gr">
-							<div class="v-thumb-img">
-								<img src="" alt="">
-							</div>
-							<h2>Lorem Ipsum Dolor Sit Amet</h2>
-							<span>2 jam lalu</span>
-						</div>
-					</a>
-					<a href="">
-						<div class="video-thumb fafa gr">
-							<div class="v-thumb-img">
-								<img src="" alt="">
-							</div>
-							<h2>Lorem Ipsum Dolor Sit AmetLorem Ipsum Dolor Sit AmetLorem Ipsum Dolor Sit Amet</h2>
-							<span>2 jam lalu</span>
-						</div>
-					</a>			
-				</div>
-			</div>
-		</div>
-	</div>
+<!-- IKLAN -->
+<div class="container">
+    <div class="box-flyad">
+        <div class="flyad">
+            <div class="m-0">
+                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <!-- Eyesoccer 4#mobileNewsDetailBackGround -->
+                <ins class="adsbygoogle"
+                     style="display:block"
+                     data-ad-client="ca-pub-7635854626605122"
+                     data-ad-slot="6050658173"
+                     data-ad-format="auto"></ins>
+                <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
+                <!-- <span style="font-size: 7em;font-weight: 100;position: fixed;text-align: center;top: 20%;color: white;">ini iklan yaa</span> -->
+                <!-- <img src="http://www.desktopimages.org/pictures/2014/0118/1/red-smoke-wallpaper-hd-5449.jpg" alt=""> -->
+            </div>
+        </div>
+    </div>
 </div>
-<!-- <div class="container-94 m-b-20 unset">
-	<span class="up-next-etube" style="margin-top: 23px;">Berita Terkini</span>
-	<div id="reqterkait" class="reqterkait">
-		<div class="etube-content etube-content-detail pd-t-10">
-			<div class="v-etube v-etube-detail">
-				<img src="http://www.desktopimages.org/pictures/2014/0118/1/red-smoke-wallpaper-hd-5449.jpg" alt="">
-			</div>
-			<h1>Lorem Ipsum Dolor Sit amet</h1>
-			<span>30 menit lalu</span>
-			<span>2000 view - 100 suka</span>
-		</div>
-		<div class="etube-content etube-content-detail">
-			<div class="v-etube v-etube-detail">
-				<img src="http://www.desktopimages.org/pictures/2014/0118/1/red-smoke-wallpaper-hd-5449.jpg" alt="">
-			</div>
-			<h1>Lorem Ipsum Dolor Sit amet Lorem Ipsum Dolor Sit amet</h1>
-			<span>30 menit lalu</span>
-			<span>2000 view - 100 suka</span>
-		</div>
-		
-	</div>
-</div> -->
+<div class="container enews-detail">
+    <div class="container m-b-15" style="margin-top: -35px !important; background-color: #fafafa;">
+        <h3 class="h3-red">VIDEO</h3>
+        <div class="carousel slide t-30 over-x">
+            <div role="listbox" class="carousel-inner w-max">
+                <div class="box item active no-decoration">
+                    <div id="reqtube" class='loadtube' action="eyetube" loading="on" clean="clstube">
+                        <div id='clstube'>
+                            <script>
+                                $(document).ready(function () {
+                                    $(window).on('load', function () {
+                                        ajaxOnLoad('loadtube');
+                                    });
+                                });
+                            </script>
+                        </div>
+                        <input type='hidden' name='fn' value='list_tube_eyenews' class='cinput'>
+                        <a href="">
+                            <div class="video-thumb fafa gr">
+                                <div class="v-thumb-img">
+                                    <img src="" alt="">
+                                </div>
+                                <h2>Lorem Ipsum Dolor Sit Amet</h2>
+                                <span>2 jam lalu</span>
+                            </div>
+                        </a>
+                        <a href="">
+                            <div class="video-thumb fafa gr">
+                                <div class="v-thumb-img">
+                                    <img src="" alt="">
+                                </div>
+                                <h2>Lorem Ipsum Dolor Sit AmetLorem Ipsum Dolor Sit AmetLorem Ipsum Dolor Sit Amet</h2>
+                                <span>2 jam lalu</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-94 m-b-20 unset">
+        <span class="up-next-etube" style="margin-top: 23px;">Berita Terkini</span>
+        <div id="reqterkait" class="reqterkait">
+            <div class="etube-content etube-content-detail pd-t-10">
+                <div class="v-etube v-etube-detail">
+                    <img src="http://www.desktopimages.org/pictures/2014/0118/1/red-smoke-wallpaper-hd-5449.jpg" alt="">
+                </div>
+                <h1>Lorem Ipsum Dolor Sit amet</h1>
+                <span>30 menit lalu</span>
+                <span>2000 view - 100 suka</span>
+            </div>
+            <div class="etube-content etube-content-detail">
+                <div class="v-etube v-etube-detail">
+                    <img src="http://www.desktopimages.org/pictures/2014/0118/1/red-smoke-wallpaper-hd-5449.jpg" alt="">
+                </div>
+                <h1>Lorem Ipsum Dolor Sit amet Lorem Ipsum Dolor Sit amet</h1>
+                <span>30 menit lalu</span>
+                <span>2000 view - 100 suka</span>
+            </div>
+
+        </div>
+    </div>
 </div>
 <!-- KOMENTAR -->
 <div class="container-comment">
-    <span class="up-next-etube" style="padding-left: 10px !important;color: #212121 !important;">Komentar</span> <span id="close" class="up-next-etube" style="float:right; padding-right: 10px;"><i class="material-icons">close</i></span>
-    <div class="fb-comments" data-href="<?="https://www.eyesoccer.id/";?><?=$_SERVER['REQUEST_URI']?>" data-numposts="5"></div>
+    <span class="up-next-etube" style="padding-left: 10px !important;color: #212121 !important;">Komentar</span> <span id="close" class="up-next-etube"
+                                                                                                                       style="float:right; padding-right: 10px;"><i
+                class="material-icons">close</i></span>
+    <div class="fb-comments" data-href="<?= "https://www.eyesoccer.id/"; ?><?= $_SERVER['REQUEST_URI'] ?>" data-numposts="5"></div>
 </div>
 <div class="box-notif">
     <div class="box-notif-content">
